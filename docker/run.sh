fdae43c0a1e85ff26a8065a8de6fc93caa61b7c2
Andras Fekete <bandi13@users.noreply.github.com>
2023-06-16 09:44:12 -0400

    Add in inswrapper_128.onnx

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Run this script from within the refacer/docker folder.
+# You'll need inswrapper_128.onnx from either:
+#    https://drive.google.com/file/d/1eu60OrRtn4WhKrzM4mQv4F3rIuyUXqfl/view?usp=drive_link
+# or https://drive.google.com/file/d/1jbDUGrADco9A1MutWjO6d_1dwizh9w9P/view?usp=sharing
+# or https://mega.nz/file/9l8mGDJA#FnPxHwpdhDovDo6OvbQjhHd2nDAk8_iVEgo3mpHLG6U
+# or https://1drv.ms/u/s!AsHA3Xbnj6uAgxhb_tmQ7egHACOR?e=CPoThO
+# or https://civitai.com/models/80324?modelVersionId=85159
 
 docker build -t refacer -f Dockerfile.nvidia . && \
     docker run --rm -v $(pwd)/out:/refacer/out -p 7860:7860 --gpus all refacer python3 app.py --server_name 0.0.0.0 && \
