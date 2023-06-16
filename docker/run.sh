fabee094d0b98ebeae277ddc06db43046050b19a
Andras Fekete <bandi13@users.noreply.github.com>
2023-06-16 09:17:34 -0400

    Save outputs on the host

--- docker/run.sh
+++ docker/run.sh
@@ -2,5 +2,5 @@
 # Run this script from within the refacer/docker folder.
 
 docker build -t refacer -f Dockerfile.nvidia . && \
-    docker run --rm -p 7860:7860 --gpus all refacer python3 app.py --server_name 0.0.0.0 && \
+    docker run --rm -v $(pwd)/out:/refacer/out -p 7860:7860 --gpus all refacer python3 app.py --server_name 0.0.0.0 && \
     sleep 1 && google-chrome --new-window "http://127.0.0.1:7860" &
